55de6549bcaa2b516ba11abb3dbb0bf8b539e447
bernabe.322922 <bernabe.322922@ANNEX-PC20>
2025-09-19 19:00:24 +0800

    Add project files.

---
+++ ThreadPriority/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadPriority
+{
+    public partial class Form1 : Form
+    {
+        Thread threadA, threadB, threadC, threadD;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                label1.Text = "-Thread Starts-";
+
+                threadA = new Thread(MyThreadClass.Thread1) { Name = "Thread A" };
+                threadB = new Thread(MyThreadClass.Thread2) { Name = "Thread B" };
+                threadC = new Thread(MyThreadClass.Thread1) { Name = "Thread C" };
+                threadD = new Thread(MyThreadClass.Thread2) { Name = "Thread D" };
+
+
+                threadA.Priority = System.Threading.ThreadPriority.Highest;
+                threadB.Priority = System.Threading.ThreadPriority.Normal;
+                threadC.Priority = System.Threading.ThreadPriority.AboveNormal;
+                threadD.Priority = System.Threading.ThreadPriority.BelowNormal;
+
+
+                threadA.Start();
+                threadB.Start();
+                threadC.Start();
+                threadD.Start();
+
+
+                threadA.Join();
+                threadB.Join();
+                threadC.Join();
+                threadD.Join();
+
+
+                label1.Text = "-End Of Threads-";
+            }
+        
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
---
+++ ThreadPriority/MyThreadClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPriority
+{
+    internal class MyThreadClass
+    {
+
+        public static void Thread1()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + i);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Thread2()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + i);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
+
